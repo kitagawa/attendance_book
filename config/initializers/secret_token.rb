7e86fea6348ffa5fa46bc7909427ed23ac0ffe4e
mkitagawa <mkitagawa@terrasky.co.jp>
2013-10-06 12:25:26 +0900

    add app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AttendanceBook::Application.config.secret_token = '5c908de7163604240cf9e6c7692919f03f533dd4c990dde8a3de73d603b16b52e0e04dab9f59fe884c1fa2e8892aab29e1f98ab191deddcc0d48f4dcfed42671'
